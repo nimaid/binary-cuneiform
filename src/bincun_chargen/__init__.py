7dc56ce8f2c1552cfd59c41605c1e435b1421f26
Ella Jameson <ellagjameson@gmail.com>
2025-09-09 18:46:51 -0700

    Add template files

---
+++ src/bincun_chargen/__init__.py
@@ -0,0 +1,5 @@
+"""Generates images of binary cuneiform characters."""
+
+__version__ = "1.0.0"
+
+from .core import run
---
+++ src/bincun_chargen/constants/links.py
@@ -0,0 +1,2 @@
+PROJECT_URL = "https://github.com/nimaid/binary-cuneiform"
+DONATE_URL = "https://www.patreon.com/nimaid"
